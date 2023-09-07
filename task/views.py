52882ceec1eb1128b52397cffbe91852656b8c9b
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2023-09-07 15:42:04 +0100

    add github file url

--- task/views.py
+++ task/views.py
@@ -14,13 +14,14 @@ def task(request):
     slack_name = request.GET.get('slack_name', None)
     track = request.GET.get('track', None)
     github_repo_url = "https://github.com/DanAdewole/hngx-stage-one.git"
+    github_file_url = "https://github.com/DanAdewole/hngx-stage-one/blob/main/manage.py"
     
     result = {
         "slack_name": slack_name,
         "current_day": current_day,
         "utc_time": current_utc_time,
         "track": track,
-        "github_file_url": "to come",
+        "github_file_url": github_file_url,
         "github_repo_url": github_repo_url,
         "status_code": 200
     }
